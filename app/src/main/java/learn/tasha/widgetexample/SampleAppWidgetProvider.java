3d7c0dcde7c3d9df32ab4c40894d56e2a6bab2df
puru chauhan <puruchauhan.tasha@gmail.com>
2017-08-26 01:15:09 +0530

    initial commit

---
+++ app/src/main/java/learn/tasha/widgetexample/SampleAppWidgetProvider.java
@@ -0,0 +1,13 @@
+package learn.tasha.widgetexample;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by tashariko on 25/08/17.
+ */
+
+public class SampleAppWidgetProvider extends AppWidgetProvider {
+
+
+
+}
