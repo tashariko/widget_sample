71cee0686d453ca2fe41e4be4c64e1d6a717463f
puru chauhan <puruchauhan.tasha@gmail.com>
2017-08-26 13:21:49 +0530

    initial commit on this branch

--- app/src/main/java/learn/tasha/widgetexample/SampleAppWidgetProvider.java
+++ app/src/main/java/learn/tasha/widgetexample/SampleAppWidgetProvider.java
@@ -13,24 +13,29 @@ import android.widget.RemoteViews;
 
 public class SampleAppWidgetProvider extends AppWidgetProvider {
 
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Create an Intent to launch MainActivity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget_simple);
+        // Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.widgetImageView, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-
-    }
-
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        // Perform any action when one or more AppWidget instances have been deleted
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Perform any action when an AppWidget for this provider is instantiated
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Perform any action when the last AppWidget instance for this provider is deleted
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
     }
 
 }
