8508c9d8b37c06b660afc2b7d0a7707c680266cf
puru chauhan <puruchauhan.tasha@gmail.com>
2017-08-26 12:27:45 +0530

    added SampleAppWidgetProvider class

--- app/src/main/java/learn/tasha/widgetexample/SampleAppWidgetProvider.java
+++ app/src/main/java/learn/tasha/widgetexample/SampleAppWidgetProvider.java
@@ -1,6 +1,11 @@
 package learn.tasha.widgetexample;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 
 /**
  * Created by tashariko on 25/08/17.
@@ -8,6 +13,24 @@ import android.appwidget.AppWidgetProvider;
 
 public class SampleAppWidgetProvider extends AppWidgetProvider {
 
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform any action when one or more AppWidget instances have been deleted
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Perform any action when an AppWidget for this provider is instantiated
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Perform any action when the last AppWidget instance for this provider is deleted
+    }
 
 }
